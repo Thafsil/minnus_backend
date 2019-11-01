0939830076555b62dffa8ceac078aeca9162c736
vijay <vijaysm97@gmail.com>
2019-11-01 11:42:46 +0530

    header fixed

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
-app.use(cors({
-    origin: '*'
-}));
+// app.use(cors({
+//     origin: '*'
+// }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-const post = require('./router/api/post')
+const post = require("./router/api/post");
 
-app.use('/api/post', post);
+app.use("/api/post", post);
 
 const port = process.env.PORT || 3000;
 
--- server/router/api/post.js
+++ server/router/api/post.js
@@ -1,44 +1,48 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const router = express.Router();
 
-mongoose.connect('mongodb+srv://thafsil:test123456@thafsil-4zp3x.mongodb.net/Thafsil?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(
+  "mongodb+srv://thafsil:test123456@thafsil-4zp3x.mongodb.net/Thafsil?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true
+  }
+);
 
 const postSchema = new mongoose.Schema({
-    name: String,
-    password: String
-})
+  name: String,
+  password: String
+});
 
-const PostModel = mongoose.model('PostModel', postSchema)
+const PostModel = mongoose.model("PostModel", postSchema);
 
 //Get
-router.get('/',  (req, res) => {
-PostModel.find({},(err,data)=>{
+router.get("/", (req, res) => {
+  PostModel.find({}, (err, data) => {
+    console.log("im in");
     res.send(data);
+  });
 });
-})
 
 //post
-router.post('/',(req, res) => {
-    PostModel.create({
-        name : req.body.name,
-    password : req.body.password
-    },(err,data)=>{
-        res.send(data).status(200);
-    })
-
-})
+router.post("/", (req, res) => {
+  PostModel.create(
+    {
+      name: req.body.name,
+      password: req.body.password
+    },
+    (err, data) => {
+      res.send(data).status(200);
+    }
+  );
+});
 
 //Delete
-router.delete('/:id', (req, res) => {
-    PostModel.deleteOne({_id: req.params.id},(err, data) => {
-        res.send(data).status(200);
-    })
-})
-
+router.delete("/:id", (req, res) => {
+  PostModel.deleteOne({ _id: req.params.id }, (err, data) => {
+    res.send(data).status(200);
+  });
+});
 
 module.exports = router;
